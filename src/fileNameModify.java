4f5a9949cc9de8386f5318d4438e2eb5ece1225c
fgyulion <287045962@qq.com>
2022-02-11 18:52:45 +0800

    modify fileNameModify

--- src/fileNameModify.java
+++ src/fileNameModify.java
@@ -1,20 +1,29 @@
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
+import java.security.Security;
 import java.util.ArrayList;
 import java.util.List;
 
 public class fileNameModify {
     static List<String> fileNames = new ArrayList<>();
+
     public static void main(String[] args) throws IOException {
         File file = new File("C:\\IdeaProject\\docs-of-guoyu");
         mkdir(file);
         getFileName(file);
         for (int i = 0; i < fileNames.size(); i++) {
-            String newFileName = fileNames.get(i).replaceAll("C:", "d:");
-            System.out.println(newFileName);
-            File newFile = new File(newFileName);
+            String temp = fileNames.get(i).replaceAll("C:", "d:");
+            StringBuffer newFileName = new StringBuffer(temp);
+            newFileName.insert(newFileName.length() - 3, "Security");
+            System.out.println(newFileName.toString());
+            File newFile = new File(newFileName.toString());
             newFile.createNewFile();
         }
+        File file1 = new File("d:\\test.java");
+        if (file1.exists()) {
+            file1.delete();
+        }
+        file1.createNewFile();
+        modifyFileContext(file1);
     }
 
     public static void getFileName(File file) {
@@ -32,7 +41,7 @@ public class fileNameModify {
         }
     }
 
-    public static void mkdir(File file){
+    public static void mkdir(File file) {
         File[] fileList = file.listFiles();
         for (int i = 0; i < fileList.length; i++) {
             if (fileList[i].isDirectory()) {
@@ -42,5 +51,16 @@ public class fileNameModify {
                 }
             }
         }
+    }
+
+    public static void modifyFileContext(File file) throws IOException {
+        FileOutputStream out = new FileOutputStream(file, true);
+        StringBuffer sb = new StringBuffer();
+        sb.append("package org.jkiss.dbeaver.ext.postgresql;\r\n");
+        sb.append("public class " + file.getName().substring(0, file.getName().length() - 5) + "Security extends " + file.getName().substring(0, file.getName().length() - 5) + " {\r\n");
+        sb.append("\r\n");
+        sb.append("}\r\n");
+        out.write(sb.toString().getBytes("utf-8"));
+        out.close();
     }
 }
